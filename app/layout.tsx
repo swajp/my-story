9c447145abf5e2e4386c7deea7e355d0053aef42
swajp <stejskalmira@icloud.com>
2023-11-02 21:34:00 +0100

    clerk added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,11 +15,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={GeistSans.className}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={GeistSans.className}>
+          <Navbar />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,8 +2,10 @@
 
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
 
 export default function Navbar() {
+  const { user } = useUser();
   return (
     <div className="z-50 bg-[#0E0E0E] static top-0 w-full p-6">
       <div className=" max-w-screen-2xl mx-auto flex items-center">
@@ -11,7 +13,15 @@ export default function Navbar() {
           my story
         </p>
         <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-          <Button className="text-white" variant={"ghost"} size={"sm"}></Button>
+          {user ? (
+            <UserButton afterSignOutUrl="/" />
+          ) : (
+            <SignInButton mode="modal">
+              <Button className="text-white" variant={"ghost"} size={"sm"}>
+                Přilásit se
+              </Button>
+            </SignInButton>
+          )}
         </div>
       </div>
     </div>
